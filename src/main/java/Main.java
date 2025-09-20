9b135d072d0e4171916828c7484f116f1e43e223
EthanShklar <eitan.shklar@gmail.com>
2025-09-20 23:41:33 +0300

    excs.10,11,12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,63 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+public class Main{
+  public static int sumSequence(int n){
+    if(n == 1){
+      return 1*2;
+    }
+    int previusSum = sumSequence(n-1);
+    if(n%2 == 0){
+      return previusSum + (n * n);
+    }
+    else{
+      return previusSum + (n*2);
+    }
+  }
+  public static void main(String [] args)
+  {
+    int n = 7;
+    int result = sumSeuence(n);
+    System.out.println("Sum of the sequence up to " + n + " is " + result);
   }
 }
+
+public class Main{
+  public static double sumOfExpression(int n){
+    return sumOfExpressionHelper(n, 1, true);
+  }
+  private static double sumOfExpressionHelper(int n, int currentOdd, boolean addOdd){
+    if(n == 0){
+      return 0;
+    }
+    if(addOdd){
+      return currentOdd + sumOfExpressionHelper(n-1, currentOdd + 2, false);
+    }
+    else{
+      return -Math.sqrt(currentOdd) + sumOfExpressionHelper(n - 1, currentOdd + 2, true);
+    }
+  }
+  public static void main(String [] args){
+    int n = 6;
+    double result = sumOfExpression(n);
+    System.out.println("Sum of first " + n " term is " + result);
+  }
+}
+
+public class Main{
+  public static int sumMultiples(int n1, int n2){
+    return sumMultiplesHelper(n1, n2, n1);
+  }
+  private static int sumMultiplesHelper( int n1, int n2, int currentMultiple){
+    if*currentMultiple >= n2){
+      return 0;
+    }
+    return currentMultiple + sumMultiplesHelper(n1, n2, currentMultiple + n1);
+  }
+
+  public static void main(String [] args){
+    int n1=3;
+    int n2 = 10;
+    int result = sumMultiples(n1, n2);
+    int result = sumMultiples(n1, n2);
+    System.out.println("Sum of multiples of " + n1 + " smaller than " + n2 + " is " + result);
+  }
+}
+  
